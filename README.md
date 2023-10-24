15099a390b301ad38b85bd7ae2c9652fa914fd9b
apolzek <viniciusgomes955@gmail.com>
2023-10-24 19:19:49 -0300

    add test: TestRemoveFile

--- README.md
+++ README.md
@@ -15,6 +15,10 @@ podman-compose up
 #or
 docker compose up
 ```
+2. 
+```sh
+migrate -path database/migrations/ -database "mysql://user:1234@tcp(0.0.0.0:3306)/go_gin_gonic?charset=utf8mb4&parseTime=True&loc=Local" u
+```
 
 2. Run app localy
 ```sh
--- database/database.go
+++ database/database.go
@@ -25,6 +25,7 @@ func ConnectDatabase() {
 	if db_config.DB_DRIVER == "mysql" {
 
 		dsnMysql := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_config.DB_USER, db_config.DB_PASSWORD, db_config.DB_HOST, db_config.DB_PORT, db_config.DB_NAME)
+		// fmt.Println(dsnMysql)
 		DB, errConnection = gorm.Open(mysql.Open(dsnMysql), &gorm.Config{})
 	}
 
